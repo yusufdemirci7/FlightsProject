9170d9ea0c1f438fcf00ed28faddb9cc9b3344a6
Yusuf Demirci <y.demirci729@gmail.com>
2024-02-20 23:08:10 +0300

    Co-authored-by: jushcmd <jushcmd@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,118 +1,349 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+// In App.js in a new project
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import * as React from 'react';
+import { View, Text, Button, TouchableOpacity, Image, StyleSheet, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { BlurView } from '@react-native-community/blur';
+import LinearGradient from 'react-native-linear-gradient';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function StartScr1({ navigation }) {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <React.Fragment>
+    <View style={[{flexDirection: 'column'}, {gap:20}]}>
+      <View 
+      style={styles.ImageContainer}>
+        <Image
+          source={require('./src/assets/images/hero-photo.png')}
+          style={styles.image} />
+          
+      </View >
+      <View style={{marginLeft: 30}}>
+
+      <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          Explore
+          </Text>
+          <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          The Beautiful
+          </Text>
+          <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          World!
+          </Text>
+
+      </View>
+      <View
+
+      
+        style={styles.ButtonContainer}>
+      
+        <TouchableOpacity
+          style={styles.buttonNext}
+          onPress={() => navigation.navigate("Opening 2")}>
+            
+          <Text style={{color: "white"}} >Next</Text>
+
+          <Image
+              source={require('./src/assets/images/Vector.png')}
+              style={styles.icon} />
+
+        </TouchableOpacity>
+
+        
+
+      </View>
     </View>
+    </React.Fragment>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function StartScr2({ navigation }) {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <React.Fragment>
+    <View style={[{flexDirection: 'column'}, {gap:20}]}>
+      <View 
+      style={styles.ImageContainer}>
+        <Image
+          source={require('./src/assets/images/hero.png')}
+          style={styles.image} />
+          
+      </View >
+      <View style={{marginLeft: 30}}>
+
+      <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          Find
+          </Text>
+          <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          Your Perfect
+          </Text>
+          <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          Tickets To Fly
+          </Text>
+
+      </View>
+      <View
+
+      
+        style={styles.ButtonContainer}>
+        
+        <TouchableOpacity 
+          style={styles.buttonBack} 
+          onPress={() => navigation.navigate("Opening 1")}>
+            
+          <Text style={{color: "black"}}>Back</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonNext}
+          onPress={() => navigation.navigate("Opening 3")}>
+            
+          <Text style={{color: "white"}} >Next</Text>
+
+          <Image
+              source={require('./src/assets/images/Vector.png')}
+              style={styles.icon} />
+
+        </TouchableOpacity>
+
+        
+
+      </View>
+    </View>
+    </React.Fragment>
+  );
+}
+
+function StartScr3({ navigation }) {
+  return (
+    <React.Fragment>
+    <View style={[{flexDirection: 'column'}, {gap: 20}]}>
+      <View 
+      style={styles.ImageContainer}>
+        <Image
+          source={require('./src/assets/images/Mask-group.png')}
+          style={styles.image} />
+          
+      </View >
+      <View style={{marginLeft: 30}}>
+
+      <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          Book
+          </Text>
+          <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          Appoinment
+          </Text>
+          <Text style={[{fontWeight: '800'}, {fontSize: 40} , {color: "black"}]}>
+          in Easiest Way!
+          </Text>
+
+      </View>
+      <View
+
+      
+        style={styles.ButtonContainer}>
+        
+        <TouchableOpacity 
+          style={styles.buttonBack} 
+          onPress={() => navigation.navigate("Opening 2")}>
+            
+          <Text style={{color: "black"}}>Back</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonNext}
+          onPress={() => navigation.navigate("Login")}>
+            
+          <Text style={{color: "white"}} >Get Started</Text>
+
+          <Image
+              source={require('./src/assets/images/Vector.png')}
+              style={styles.icon} />
+
+        </TouchableOpacity>
+
+        
+
+      </View>
+    </View>
+    </React.Fragment>
+
+  );
+
+}
+
+function LoginScr ({ navigation }){
+  return(
+  
+  <View style={[styles.ImageContainer, {gap: 70}, {marginTop: 150}] }>
+    
+    <View style={[styles.Login , {flexDirection: 'row'}]}>
+      <Image 
+        source={require('./src/assets/images/email.png')}
+        style={styles.email}>
+        
+      </Image>
+      <TextInput placeholder='Email'></TextInput> 
+      
+
+    </View>
+
+    <View style={[styles.Login , {flexDirection: 'row'}]}> 
+    <Image 
+        source={require('./src/assets/images/Password.png')}
+        style={styles.email}>
+        
+      </Image>
+      <TextInput placeholder='Password'></TextInput> 
+      
+
+    </View>
+
+    <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+      <Text>Sign Up</Text> 
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => navigation.navigate("Opening 3")}>
+      <Text>Back</Text> 
+      </TouchableOpacity>
+    
+  </View>
+  );
+}
+
+function SignUpScr ({navigation }){
+  return(
+    <View style={{flex:1}}>
+    {/* Background gradient */}
+    <LinearGradient
+      colors={['#FFFFFF', '#40E0D0']}
+      style={{flex:1}}
+    >
+      {/* Blur effect */}
+      <BlurView
+        style={styles.blurView}
+        blurType="light"
+        blurAmount={10}
+      >
+        {/* Content */}
+        
+      </BlurView>
+    </LinearGradient>
+  </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}>
+
+        <Stack.Screen name="Opening 1" component={StartScr1} />
+
+        <Stack.Screen name="Opening 2" component={StartScr2} />
+
+        <Stack.Screen name="Opening 3" component={StartScr3} />
+
+        <Stack.Screen name="Login" component={LoginScr} />
+
+        <Stack.Screen name="SignUp" component={SignUpScr} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+  ImageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  ButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+    gap:10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  icon: {
+    width: 15,
+    height: 15,
+    resizeMode: 'cover', // or 'contain', 'stretch', 'repeat', 'center'
+    borderRadius: 10,
+    borderWidth: 2,
+    
   },
-  highlight: {
-    fontWeight: '700',
+
+  image: {
+    width: 360,
+    height: 400,
+    resizeMode: 'cover', // or 'contain', 'stretch', 'repeat', 'center'
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'white',
+    // You can apply more styles as needed
+  },
+  buttonNext: {
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00D2C5',
+    color: "#fff",
+    margin: 7,
+    borderRadius: 10,
+    flexDirection: 'row',
+    gap: 15,
+  },
+
+  buttonBack: {
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: "#fff",
+    margin: 7,
+    borderRadius: 10,
+
+  },
+
+  Login: {
+    width: 327,
+    height: 56,
+    borderRadius: 500,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: 5,
+  },
+
+  email: {
+    backgroundColor: '#fff',
+    marginLeft: 10,
+  },
+
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  blurView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
 export default App;
+
+
